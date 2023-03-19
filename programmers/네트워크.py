f440d0127c21728a76abcc2c845e601518c5fd35
mrsuit0114 <woosa0114@gmail.com>
2023-03-19 12:48:46 +0900

    게임맵최단거리, bfs

--- programmers/네트워크.py
+++ programmers/네트워크.py
@@ -2,7 +2,6 @@ from collections import deque
 
 def solution(n, computers):
     answer = 0
-    parent = [0] * n # 부모 테이블 초기화하기
     # graph = [[]] * n  이렇게하면 배열이 개별적으로 되는게 아닌거같네
     graph = [[] for i in range(n)]  # 참..
 
@@ -32,3 +31,24 @@ def solution(n, computers):
     return answer
 
 print(solution(3,[[1, 1, 0], [1, 1, 0], [0, 0, 1]]))
+
+# def solution(n, computers):
+#     answer = 0
+#     visited = [0 for i in range(n)]
+#     def dfs(computers, visited, start):
+#         stack = [start]
+#         while stack:
+#             j = stack.pop()
+#             if visited[j] == 0:
+#                 visited[j] = 1
+#             # for i in range(len(computers)-1, -1, -1):
+#             for i in range(0, len(computers)):
+#                 if computers[j][i] ==1 and visited[i] == 0:
+#                     stack.append(i)
+#     i=0
+#     while 0 in visited:
+#         if visited[i] ==0:
+#             dfs(computers, visited, i)
+#             answer +=1
+#         i+=1
+#     return answer
