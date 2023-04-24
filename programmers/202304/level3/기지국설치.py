7c4c8d4cb9cbf0dd83c61148c097ac5c043eefca
mrsuit0114 <woosa0114@gmail.com>
2023-04-24 14:31:38 +0900

    수학

---
+++ programmers/202304/level3/기지국설치.py
@@ -0,0 +1,28 @@
+from collections import deque
+import math
+
+def solution(n, stations, w):
+    answer = 0
+    last = 1 # 전파안받기 시작하는 지점
+    dq = deque(stations)
+    while last<=n:
+        if dq:
+            tmp = dq.popleft() # station의 중심
+            tmpl = max(0,tmp-w) # station의 영향 최소지점
+            tmpr = tmp+w
+            answer += math.ceil((tmpl-last)/(2*w+1))
+            last = tmpr + 1
+        else:
+            answer+=math.ceil((n-last+1)/(2*w+1))
+            return answer
+        
+    # if last == n:  while에서 등호붙힌거랑 뭐가 차이가나서 테스트 13결과가 달라지냐?
+    #       answer+=1 last==n일 때 dq에서 처리했을 수도 있는데 더하면 달라지네
+
+    return answer
+
+print(solution(3,[1,2,3],1))
+
+# 이분탐색인가? 비슷한 문제 봄 아닌거같기도하고?
+# station에 따라 구간들이 나뉘어있음
+# station의 영향시작지점과 마지막 기지국 영향지점 구간의 몫을 올림하고  
