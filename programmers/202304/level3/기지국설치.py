d46bf612778000a7289091cb69b822c2f0d78821
mrsuit0114 <woosa0114@gmail.com>
2023-04-24 14:37:17 +0900

    수학, 나눗셈보다 곱셈으로 푸는게 효율이 좋음

--- programmers/202304/level3/기지국설치.py
+++ programmers/202304/level3/기지국설치.py
@@ -21,7 +21,27 @@ def solution(n, stations, w):
 
     return answer
 
+# 다른사람 풀이 난 몫을구해서한거고 풀이는 곱으로해서함 나눗셈보다 곱셈이 빨라서 그런가봄
+
+def solution(n, stations, w):
+    ans = 0
+    idx = 0
+    location = 1
+
+    while(location <= n) :
+        if(idx < len(stations) and location >= stations[idx]-w) :
+            location = stations[idx]+w+1
+            idx += 1
+        else :
+            location += 2*w+1
+            ans += 1
+    return ans
+
+
 print(solution(3,[1,2,3],1))
+
+
+
 
 # 이분탐색인가? 비슷한 문제 봄 아닌거같기도하고?
 # station에 따라 구간들이 나뉘어있음
